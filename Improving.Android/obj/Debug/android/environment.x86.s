37417e3dfd2c76d19ad6e73ad5f68d1f25cb290f
Sirfouq <kokkinos1@gmail.com>
2022-02-10 20:32:37 +0200

    Improved Version of previous project

--- Improving.Android/obj/Debug/android/environment.x86.s
+++ Improving.Android/obj/Debug/android/environment.x86.s
@@ -71,7 +71,7 @@ mono_aot_mode_name:
 	.section	.rodata.env.str.8,"aMS",@progbits,1
 	.type	.L.env.str.8, @object
 .L.env.str.8:
-	.asciz	"53d54ade-d32c-4f51-9a6d-7fd2c742b2b1"
+	.asciz	"88aba1e6-42fa-491a-8140-7dc65065f3cf"
 	.size	.L.env.str.8, 37
 	.section	.rodata.env.str.9,"aMS",@progbits,1
 	.type	.L.env.str.9, @object
